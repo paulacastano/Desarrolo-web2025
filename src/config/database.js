f1e9ab481043c3f71c69a16e86984e9c81c05cfb
Paula Castano <paula.castano1@utp.edu.co>
2025-03-05 19:44:07 -0500

    Se crean nuevos archivos para la conexion a bd

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = requiere('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false,
+    timezone: '-05:00'
+});
+
+module.exports = sequelize;
---
+++ src/config/dotenv.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    JWT_SECRET: process.env.JWT_SECRET
+}
